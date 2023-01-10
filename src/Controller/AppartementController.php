2990278e5c525111e80a916686d1fdc638a66fec
nathanCahgn <nathan14.cahaigne@gmail.com>
2023-01-10 09:22:07 +0100

    création vue appartement/lister.html.twig

--- src/Controller/AppartementController.php
+++ src/Controller/AppartementController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Appartement;
 
 class AppartementController extends AbstractController
